767d9db34b2081b92affb263c0b6ad43620c5be0
kululenko <alex.pmail@yahoo.com>
2023-12-08 21:16:30 +0100

    added plots

--- data_analysis_psql.py
+++ data_analysis_psql.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
 
 engine = create_engine("postgresql://postgres:sihing123@localhost:5432/salesanalysis")
 
@@ -46,3 +51,45 @@ df3 = pd.read_sql(query3, engine)
 print(df3)
 df4 = pd.read_sql(query4, engine)
 print(df4)
+
+
+#visualising my querys // Gesamtumsatz pro Produkt
+plt.figure(figsize=(10,6))
+sns.barplot(x="product_id", y="total_sales", data=df2)
+plt.title("Gesamtumsatz pro Produkt")
+plt.xlabel("Product_ID")
+plt.ylabel("Gesamtumsatz")
+plt.xticks(rotation=45)
+plt.show()
+
+#Durchschnittlicher Verkaufswert pro Tag
+plt.figure(figsize=(10,6))
+sns.lineplot(x="sale_date", y="average_daily_sales", data=df3)
+plt.title("Durchschnittlicher Verkaufswert pro Tag")
+plt.xlabel("Datum")
+plt.ylabel("Durchschnittlicher Verkaufswert")
+plt.xticks(rotation=45)
+plt.show()
+
+#Top Verkaufstage
+plt.figure(figsize=(10,6))
+sns.barplot(x="sale_date", y="total_sales", data=df4)
+plt.title("Top Verkaufstage")
+plt.xlabel("Datum")
+plt.ylabel("Gesamtumsatz")
+plt.xticks(rotation=45)
+plt.show()
+
+#showing top sales and lowest sales
+top10 = df2.head(10)
+bot10 = df2.tail(10)
+
+topbot_10 = pd.concat([top10,bot10])
+
+plt.figure(figsize=(12,8))
+sns.barplot(x="product_id", y="total_sales", data=topbot_10)
+plt.title("Top 10 and bot nach Umsatz")
+plt.xlabel("Product_id")
+plt.ylabel("Gesamtumsatz")
+plt.xticks(rotation=90)
+plt.show() 
--- machine_learning1.py
+++ machine_learning1.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sqlalchemy import create_engine
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error,r2_score
 
-
+# machine learning
 engine = create_engine("postgresql://postgres:sihing123@localhost:5432/salesanalysis")
 
 df = pd.read_sql("SELECT * FROM sales", engine)
@@ -23,3 +25,19 @@ y_pred = model.predict(X_test)
 print("Mean Squared Error:", mean_squared_error(y_test,y_pred))
 print("Coefficient of dtermination (R^2):", r2_score(y_test,y_pred))
 
+
+print(df.describe())
+
+
+sns.histplot(df["price"], kde=True)
+plt.show()
+
+sns.countplot(x="product_id", data=df)
+plt.show()
+
+corr = df.corr()
+sns.heatmap(corr, annot=True, cmap="coolwarm")
+plt.show()
+
+sns.pairplot(df)
+plt.show()
